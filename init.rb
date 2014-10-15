cafe24da7536832144f202f804556e4bd4d54647
Will Leinweber <will@bitfission.com>
2014-10-15 14:20:51 -0700

    make it actually work

--- init.rb
+++ init.rb
@@ -4,11 +4,16 @@ class Heroku::Auth
     alias_method :_orig_ask_for_second_factor, :ask_for_second_factor
     def ask_for_second_factor
       dialog = `osascript -e 'tell app "System Events" to display dialog "Two Factor:" default answer "" with icon 1 with hidden answer buttons "ugh" default button 1'`
-      result = dialog.match(/text returned:(\w+)/)[1]
+      match = dialog.match(/text returned:(\w+)/)
+
+      @two_factor_code = match && match[1]
+      @api=nil
+
       file = File.expand_path('~/.heroku/yubipresses.log')
       FileUtils.touch file
       open(file, 'a') { |f| f.puts Time.now.utc.to_s }
-      result
+
+      @two_factor_code
     end
   end
 end
