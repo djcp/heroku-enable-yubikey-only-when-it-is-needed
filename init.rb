cafebbebe2dd5ba3207edc08550243dde04a3ef0
Will Leinweber <will@bitfission.com>
2014-10-15 14:11:05 -0700

    use a popup

--- init.rb
+++ init.rb
@@ -3,9 +3,8 @@ class Heroku::Auth
   class << self
     alias_method :_orig_ask_for_second_factor, :ask_for_second_factor
     def ask_for_second_factor
-      `osascript -e 'tell application "yubiswitch" to KeyOn'`
-      result = _orig_ask_for_second_factor
-      `osascript -e 'tell application "yubiswitch" to KeyOff'`
+      dialog = `osascript -e 'tell app "System Events" to display dialog "Two Factor:" default answer "" with icon 1 with hidden answer buttons "ugh" default button 1'`
+      result = dialog.match(/text returned:(\w+)/)[1]
       file = File.expand_path('~/.heroku/yubipresses.log')
       FileUtils.touch file
       open(file, 'a') { |f| f.puts Time.now.utc.to_s }
