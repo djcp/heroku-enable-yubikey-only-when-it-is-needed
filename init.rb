cafe6b14deceab522fdbf96c8ddcff783c45e9ab
Will Leinweber <will@bitfission.com>
2014-10-20 10:54:35 -0700

    I dont use the yubikey thing anymore, but I want logging

--- init.rb
+++ init.rb
@@ -3,9 +3,7 @@ class Heroku::Auth
   class << self
     alias_method :_orig_ask_for_second_factor, :ask_for_second_factor
     def ask_for_second_factor
-      `osascript -e 'tell application "yubiswitch" to KeyOn'`
       result = _orig_ask_for_second_factor
-      `osascript -e 'tell application "yubiswitch" to KeyOff'`
       file = File.expand_path('~/.heroku/yubipresses.log')
       FileUtils.touch file
       open(file, 'a') { |f| f.puts Time.now.utc.to_s }
