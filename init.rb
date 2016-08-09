3b251468bddf9e8ebb62285adc4f7c11d1a0b631
Dan Collis-Puro <dan@collispuro.net>
2016-08-09 15:44:59 -0400

    Use the yubitoggle interface preferentially

--- init.rb
+++ init.rb
@@ -1,16 +1,26 @@
 require 'fileutils'
+require 'mkmf'
+
 class Heroku::Auth
   class << self
     alias_method :_orig_ask_for_second_factor, :ask_for_second_factor
     def ask_for_second_factor
       if RUBY_PLATFORM.include? "linux"
-        `yubiswitch on`
+        if find_executable "yubitoggle"
+          `yubitoggle --on`
+        else
+          `yubiswitch on`
+        end
       elsif RUBY_PLATFORM.include? "darwin"
         `osascript -e 'tell application "yubiswitch" to KeyOn'`
       end
       result = _orig_ask_for_second_factor
       if RUBY_PLATFORM.include? "linux"
-        `yubiswitch off`
+        if find_executable "yubitoggle"
+          `yubitoggle --off`
+        else
+          `yubiswitch off`
+        end
       elsif RUBY_PLATFORM.include? "darwin"
         `osascript -e 'tell application "yubiswitch" to KeyOff'`
       end
